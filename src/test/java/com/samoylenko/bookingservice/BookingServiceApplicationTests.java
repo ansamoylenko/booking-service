e4e1665b6c5b7328fac252e7de582e02a0d4dcb9
asamoylenko <a.samoylenko@spbu.ru>
2024-05-19 00:22:28 +0300

    Initial commit

---
+++ src/test/java/com/samoylenko/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.samoylenko.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
